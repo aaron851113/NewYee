dd1133af6719322db3094c82e0ae4b843ec9e424
Aaron Chou <48345009+aaron851113@users.noreply.github.com>
2020-04-06 19:23:37 +0800

    Add files via upload

--- OnlineInference/src/violation_demo.py
+++ OnlineInference/src/violation_demo.py
@@ -4,33 +4,38 @@ import torch
 import time
 import numpy as np
 
-def save_violation(violation_frame,img_stack,img_result,frame_num):
-    if violation_frame != -1 :
+
+def save_violation(violation_frame,img_stack,img_result,frame_num,check_frame,vio):
+    if len(img_stack) >= frame_num *2:
+        img_stack = img_stack[1:]
+        img_stack.append(img_result) #只保留最新frame_num筆img_result
+    else:   
         img_stack.append(img_result)
+    if violation_frame != -1 :
         f = frame_num
-        index = [0,f//2,f,int(f*1.5),f*2]
-        # 且湊滿frame_num*2張frame，更改旗標，清空
-        if len(img_stack) == f*2+1 :
+        index = [0,f//2,f,int(f*1.5),f*2-1]
+
+        if len(img_stack) == f*2 and (check_frame%frame_num==0):
             tmp_path = './violation/'+str(violation_frame)+'/'
             for i in index :
                 save_index = violation_frame-f+i
                 img_path = tmp_path + str(save_index)+'.png'
                 im_name = str(violation_frame)
                 cv2.imwrite(img_path,img_stack[i])
-                
-            f = frame_num*-1
-            img_stack = img_stack[f:]
+            
             violation_frame = -1
 
-    # 如果沒有違規        
-    else : 
-        if len(img_stack) < frame_num :
-            img_stack.append(img_result)
-        else:
-            f = (frame_num*-1)+1
-            img_stack = img_stack[f:] 
-            img_stack.append(img_result) #只保留最新frame_num筆img_result
-    return violation_frame,img_stack
+        check_frame+=1
+        vio = True
+        if check_frame>frame_num-1:
+            check_frame=0
+        return violation_frame,img_stack,check_frame,vio
+    
+    if vio :
+        check_frame+=1
+    if check_frame>frame_num-1:
+        check_frame=0
+    return violation_frame,img_stack,check_frame,vio
     
     
 def createFolder(directory):
